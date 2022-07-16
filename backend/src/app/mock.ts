e2670428b60f279eb00069fa3d8468f72b901951
renanlimaleite <renan.lima.softwares@gmail.com>
2022-07-16 14:46:34 -0300

    add: mock

---
+++ backend/src/app/mock.ts
@@ -0,0 +1,149 @@
+const Mock = [
+  {
+    id: '1',
+    type: 'main',
+    categorie: 'economia',
+    title: 'Quem não tiver valores a receber nesta etapa poderá ter nas próximas fases, diz BC',
+    content: [
+      {
+        resume: 'Nesta primeira fase do serviço são cerca de R$ 4 bilhões a serem devolvidos. Banco Central estima que os clientes tenham a receber cerca de R$ 8 bilhões.',
+        date: '13/02/2022 as 16h30, Por: Redação',
+        text: 'economia 1) Aliquam feugiat dolor sit amet sagittis rhoncus. Fusce tincidunt metus vitae nisl bibendum, at condimentum lectus convallis. Suspendisse lobortis convallis ante at sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam ac ipsum maximus, rutrum sem eu, auctor ante. Pellentesque maximus nisi a malesuada semper. In at mattis lacus. Vivamus lobortis orci volutpat est tempus dictum. Nulla dapibus lectus nibh. Sed ac condimentum ligula. Donec gravida, sapien suscipit lobortis elementum, purus libero viverra ante, a maximus tellus elit et ex.',
+      },
+    ],
+  },
+  {
+    id: '2',
+    type: 'main',
+    categorie: 'educacao',
+    title: 'Datafolha: Após ensino remoto, 76% precisam de reforço na alfabetização',
+    img: 'https://www.cnte.org.br/images/stories/2022/2022_06_30_site_getty_img.jpg',
+    content: [
+      {
+        resume: 'Nesta primeira fase do serviço são cerca de R$ 4 bilhões a serem devolvidos. Banco Central estima que os clientes tenham a receber cerca de R$ 8 bilhões.',
+        date: '13/02/2022 as 16h30, Por: Redação',
+        text: 'educacao 2) Aliquam feugiat dolor sit amet sagittis rhoncus. Fusce tincidunt metus vitae nisl bibendum, at condimentum lectus convallis. Suspendisse lobortis convallis ante at sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam ac ipsum maximus, rutrum sem eu, auctor ante. Pellentesque maximus nisi a malesuada semper. In at mattis lacus. Vivamus lobortis orci volutpat est tempus dictum. Nulla dapibus lectus nibh. Sed ac condimentum ligula. Donec gravida, sapien suscipit lobortis elementum, purus libero viverra ante, a maximus tellus elit et ex.',
+      },
+    ],
+  },
+  {
+    id: '3',
+    type: 'main',
+    categorie: 'diversidades',
+    title: 'Lotomania: com prêmio de R$ 5 milhões, veja os números sorteados hoje',
+    img: 'https://www.ifpb.edu.br/materias/diversidade-sera-tema-de-campanhas-realizadas-pela-prae/diversidade-5.jpg/@@images/image.jpeg',
+    content: [
+      {
+        resume: 'Nesta primeira fase do serviço são cerca de R$ 4 bilhões a serem devolvidos. Banco Central estima que os clientes tenham a receber cerca de R$ 8 bilhões.',
+        date: '13/02/2022 as 16h30, Por: Redação',
+        text: 'diversidades 3) Aliquam feugiat dolor sit amet sagittis rhoncus. Fusce tincidunt metus vitae nisl bibendum, at condimentum lectus convallis. Suspendisse lobortis convallis ante at sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam ac ipsum maximus, rutrum sem eu, auctor ante. Pellentesque maximus nisi a malesuada semper. In at mattis lacus. Vivamus lobortis orci volutpat est tempus dictum. Nulla dapibus lectus nibh. Sed ac condimentum ligula. Donec gravida, sapien suscipit lobortis elementum, purus libero viverra ante, a maximus tellus elit et ex.',
+      },
+    ],
+  },
+  {
+    id: '4',
+    type: 'secondary',
+    categorie: 'economia',
+    title: 'Quem não tiver valores a receber nesta etapa poderá ter nas próximas fases, diz BC',
+    content: [
+      {
+        resume: 'Nesta primeira fase do serviço são cerca de R$ 4 bilhões a serem devolvidos. Banco Central estima que os clientes tenham a receber cerca de R$ 8 bilhões.',
+        date: '13/02/2022 as 16h30, Por: Redação',
+        text: 'economia 4) Aliquam feugiat dolor sit amet sagittis rhoncus. Fusce tincidunt metus vitae nisl bibendum, at condimentum lectus convallis. Suspendisse lobortis convallis ante at sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam ac ipsum maximus, rutrum sem eu, auctor ante. Pellentesque maximus nisi a malesuada semper. In at mattis lacus. Vivamus lobortis orci volutpat est tempus dictum. Nulla dapibus lectus nibh. Sed ac condimentum ligula. Donec gravida, sapien suscipit lobortis elementum, purus libero viverra ante, a maximus tellus elit et ex.',
+      },
+    ],
+  },
+  {
+    id: '5',
+    type: 'secondary',
+    categorie: 'economia',
+    title: 'Quem não tiver valores a receber nesta etapa poderá ter nas próximas fases, diz BC',
+    content: [
+      {
+        resume: 'Nesta primeira fase do serviço são cerca de R$ 4 bilhões a serem devolvidos. Banco Central estima que os clientes tenham a receber cerca de R$ 8 bilhões.',
+        date: '13/02/2022 as 16h30, Por: Redação',
+        text: 'economia 5) Aliquam feugiat dolor sit amet sagittis rhoncus. Fusce tincidunt metus vitae nisl bibendum, at condimentum lectus convallis. Suspendisse lobortis convallis ante at sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam ac ipsum maximus, rutrum sem eu, auctor ante. Pellentesque maximus nisi a malesuada semper. In at mattis lacus. Vivamus lobortis orci volutpat est tempus dictum. Nulla dapibus lectus nibh. Sed ac condimentum ligula. Donec gravida, sapien suscipit lobortis elementum, purus libero viverra ante, a maximus tellus elit et ex.',
+      },
+    ],
+  },
+  {
+    id: '6',
+    type: 'secondary',
+    categorie: 'economia',
+    title: 'Quem não tiver valores a receber nesta etapa poderá ter nas próximas fases, diz BC',
+    content: [
+      {
+        resume: 'Nesta primeira fase do serviço são cerca de R$ 4 bilhões a serem devolvidos. Banco Central estima que os clientes tenham a receber cerca de R$ 8 bilhões.',
+        date: '13/02/2022 as 16h30, Por: Redação',
+        text: 'economia 6) Aliquam feugiat dolor sit amet sagittis rhoncus. Fusce tincidunt metus vitae nisl bibendum, at condimentum lectus convallis. Suspendisse lobortis convallis ante at sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam ac ipsum maximus, rutrum sem eu, auctor ante. Pellentesque maximus nisi a malesuada semper. In at mattis lacus. Vivamus lobortis orci volutpat est tempus dictum. Nulla dapibus lectus nibh. Sed ac condimentum ligula. Donec gravida, sapien suscipit lobortis elementum, purus libero viverra ante, a maximus tellus elit et ex.',
+      },
+    ],
+  },
+  {
+    id: '7',
+    type: 'secondary',
+    categorie: 'economia',
+    title: 'Quem não tiver valores a receber nesta etapa poderá ter nas próximas fases, diz BC',
+    content: [
+      {
+        resume: 'Nesta primeira fase do serviço são cerca de R$ 4 bilhões a serem devolvidos. Banco Central estima que os clientes tenham a receber cerca de R$ 8 bilhões.',
+        date: '13/02/2022 as 16h30, Por: Redação',
+        text: 'economia 7) Aliquam feugiat dolor sit amet sagittis rhoncus. Fusce tincidunt metus vitae nisl bibendum, at condimentum lectus convallis. Suspendisse lobortis convallis ante at sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam ac ipsum maximus, rutrum sem eu, auctor ante. Pellentesque maximus nisi a malesuada semper. In at mattis lacus. Vivamus lobortis orci volutpat est tempus dictum. Nulla dapibus lectus nibh. Sed ac condimentum ligula. Donec gravida, sapien suscipit lobortis elementum, purus libero viverra ante, a maximus tellus elit et ex.',
+      },
+    ],
+  },
+  {
+    id: '8',
+    type: 'secondary',
+    categorie: 'educacao',
+    title: 'Datafolha: Após ensino remoto, 76% precisam de reforço na alfabetização',
+    content: [
+      {
+        resume: 'Nesta primeira fase do serviço são cerca de R$ 4 bilhões a serem devolvidos. Banco Central estima que os clientes tenham a receber cerca de R$ 8 bilhões.',
+        date: '13/02/2022 as 16h30, Por: Redação',
+        text: 'educacao 8) Aliquam feugiat dolor sit amet sagittis rhoncus. Fusce tincidunt metus vitae nisl bibendum, at condimentum lectus convallis. Suspendisse lobortis convallis ante at sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam ac ipsum maximus, rutrum sem eu, auctor ante. Pellentesque maximus nisi a malesuada semper. In at mattis lacus. Vivamus lobortis orci volutpat est tempus dictum. Nulla dapibus lectus nibh. Sed ac condimentum ligula. Donec gravida, sapien suscipit lobortis elementum, purus libero viverra ante, a maximus tellus elit et ex.',
+      },
+    ],
+  },
+  {
+    id: '9',
+    type: 'secondary',
+    categorie: 'educacao',
+    title: 'Datafolha: Após ensino remoto, 76% precisam de reforço na alfabetização',
+    content: [
+      {
+        resume: 'Nesta primeira fase do serviço são cerca de R$ 4 bilhões a serem devolvidos. Banco Central estima que os clientes tenham a receber cerca de R$ 8 bilhões.',
+        date: '13/02/2022 as 16h30, Por: Redação',
+        text: 'educacao 9) Aliquam feugiat dolor sit amet sagittis rhoncus. Fusce tincidunt metus vitae nisl bibendum, at condimentum lectus convallis. Suspendisse lobortis convallis ante at sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam ac ipsum maximus, rutrum sem eu, auctor ante. Pellentesque maximus nisi a malesuada semper. In at mattis lacus. Vivamus lobortis orci volutpat est tempus dictum. Nulla dapibus lectus nibh. Sed ac condimentum ligula. Donec gravida, sapien suscipit lobortis elementum, purus libero viverra ante, a maximus tellus elit et ex.',
+      },
+    ],
+  },
+  {
+    id: '10',
+    type: 'secondary',
+    categorie: 'diversidades',
+    title: 'Lotomania: com prêmio de R$ 5 milhões, veja os números sorteados',
+    content: [
+      {
+        resume: 'Nesta primeira fase do serviço são cerca de R$ 4 bilhões a serem devolvidos. Banco Central estima que os clientes tenham a receber cerca de R$ 8 bilhões.',
+        date: '13/02/2022 as 16h30, Por: Redação',
+        text: 'diversidades 10) Aliquam feugiat dolor sit amet sagittis rhoncus. Fusce tincidunt metus vitae nisl bibendum, at condimentum lectus convallis. Suspendisse lobortis convallis ante at sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam ac ipsum maximus, rutrum sem eu, auctor ante. Pellentesque maximus nisi a malesuada semper. In at mattis lacus. Vivamus lobortis orci volutpat est tempus dictum. Nulla dapibus lectus nibh. Sed ac condimentum ligula. Donec gravida, sapien suscipit lobortis elementum, purus libero viverra ante, a maximus tellus elit et ex.',
+      },
+    ],
+  },
+  {
+    id: '11',
+    type: 'secondary',
+    categorie: 'diversidades',
+    title: 'Lotomania: com prêmio de R$ 5 milhões, veja os números sorteados',
+    content: [
+      {
+        resume: 'Nesta primeira fase do serviço são cerca de R$ 4 bilhões a serem devolvidos. Banco Central estima que os clientes tenham a receber cerca de R$ 8 bilhões.',
+        date: '13/02/2022 as 16h30, Por: Redação',
+        text: 'diversidades 11) Aliquam feugiat dolor sit amet sagittis rhoncus. Fusce tincidunt metus vitae nisl bibendum, at condimentum lectus convallis. Suspendisse lobortis convallis ante at sagittis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam ac ipsum maximus, rutrum sem eu, auctor ante. Pellentesque maximus nisi a malesuada semper. In at mattis lacus. Vivamus lobortis orci volutpat est tempus dictum. Nulla dapibus lectus nibh. Sed ac condimentum ligula. Donec gravida, sapien suscipit lobortis elementum, purus libero viverra ante, a maximus tellus elit et ex.',
+      },
+    ],
+  },
+];
+
+export default Mock;
